42b97ba822247cfb0633a96f7771be49117db905
Jax Drummond <jaxdrummond9@gmail.com>
2025-09-29 10:37:34 -0400

    Added Docstrings to factorial.pu

--- factorial.py
+++ factorial.py
@@ -5,6 +5,15 @@ import time
 final_list = []
 
 def factorial(n):
+    """
+    This function gets the factorial of a number
+
+    Args:
+        n (int): The number
+
+    Returns:
+        int: the factorial of the inputted number
+    """
 
     time.sleep(.1)
 
@@ -17,6 +26,15 @@ def factorial(n):
     return factorial
 
 def sum_factorial():
+    """
+    This function gets the sum factorial of a number
+
+    Args:
+        n (int): The number
+
+    Returns:
+        int: the sum factorial of the inputted number
+    """
 
     for i in range(50):
 
